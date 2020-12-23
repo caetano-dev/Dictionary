8f86a426d4e2115937bdef274fe82d3baf657a54
Pedro Pires <pcaetanop@gmail.com>
2020-12-23 11:18:13 -0300

    Adding definition h2

--- script.js
+++ script.js
@@ -2,29 +2,32 @@ const inputElement = document.getElementById("input");
 const buttonElement = document.getElementById("button");
 const definitionDiv = document.getElementById("definition");
 
-buttonElement.addEventListener("click", () => showDefinitions());
+buttonElement.addEventListener("click", () => showDefinitions())
 
 async function getAPI() {
   const response = await fetch(
     `https://api.dictionaryapi.dev/api/v2/entries/en/${inputElement.value}`,
     {
-      method: "GET", 
+      method: "GET",
     }
   );
   const results = await response.json();
-  return results[0].meanings[0].definitions[0].definition
+  return results[0].meanings[0].definitions[0].definition;
 }
 async function showDefinitions() {
   definitionDiv.innerHTML = "";
-  var definitionText = document.createElement("h3");
+  const definitionText = document.createElement("h3");
 
   if (inputElement.value != "") {
     const definitionResults = await getAPI();
-    var text = document.createTextNode(definitionResults);
+    const text = document.createTextNode(definitionResults);
+    const h2 = document.createElement("h2");
+    h2.textContent = "Definition:";
+    definitionDiv.appendChild(h2);
     definitionText.appendChild(text);
     definitionDiv.appendChild(definitionText);
   } else {
-    var text = document.createTextNode("I need you to type something :(");
+    const text = document.createTextNode("I need you to type something :(");
     definitionText.appendChild(text);
     definitionDiv.appendChild(definitionText);
   }
