246735c83f3b4b5052c27603ab12c76c78c04e22
Pedro Pires <pcaetanop@gmail.com>
2021-01-05 17:52:52 -0300

    Adding switch for definition h2

--- script.js
+++ script.js
@@ -1,26 +1,27 @@
 const inputElement = document.getElementById("input");
 const buttonElement = document.getElementById("button");
 const definitionDiv = document.getElementById("definition");
+const h2 = document.createElement("h2");
 
 let language;
 const pathname = document.location.pathname;
 switch (pathname) {
   case "/index.html":
     language = "en";
-    console.log("set to english");
+    h2.textContent = "Definition:";
     break;
   case "/portuguese.html":
     language = "pt-BR";
-    console.log("set to pt");
+    h2.textContent = "Definição:";
     break;
   case "/french.html":
     language = "fr";
-    console.log("set to fr");
+    h2.textContent = "Définition:";
     break;
   default:
     language = "en";
+    h2.textContent = "Definition:";
 }
-
 buttonElement.addEventListener("click", () => showDefinitions());
 
 inputElement.addEventListener("keyup", function (event) {
@@ -46,14 +47,24 @@ async function showDefinitions() {
   if (inputElement.value) {
     const definitionResults = await getAPI();
     const text = document.createTextNode(definitionResults);
-    const h2 = document.createElement("h2");
-    h2.textContent = "Definition:";
     definitionDiv.appendChild(h2);
     definitionText.appendChild(text);
     definitionDiv.appendChild(definitionText);
   } else {
-    const text = document.createTextNode("I need you to type something :(");
-    definitionText.appendChild(text);
-    definitionDiv.appendChild(definitionText);
+    if (language == "en") {
+      const text = document.createTextNode("I need you to type something :(");
+      definitionText.appendChild(text);
+      definitionDiv.appendChild(definitionText);
+    } else if (language == "pt-BR") {
+      const text = document.createTextNode("Preciso que você digite algo :(");
+      definitionText.appendChild(text);
+      definitionDiv.appendChild(definitionText);
+    } else if (language == "fr") {
+      const text = document.createTextNode(
+        "J'ai besoin que vouz tapiez quelque chose :("
+      );
+      definitionText.appendChild(text);
+      definitionDiv.appendChild(definitionText);
+    }
   }
 }
